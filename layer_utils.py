40e39317749847dad265475229a89716fd5f1fde
harrison-usyd <harrison.nguyen12@gmail.com>
2018-08-17 17:10:31 +1000

    added 3d layers

--- layer_utils.py
+++ layer_utils.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.contrib.layers import xavier_initializer_conv2d
+from tensorflow.contrib.layers import xavier_initializer_conv2d,xavier_initializer
 
 def conv_layer(x,filters,kernel_size,strides,activation,training_ph,
                 batch_norm='batch',padding='same'):
@@ -66,3 +66,47 @@ def residual_block(x, filters,training_ph,kernel_size=3, stride=1,activation=tf.
         output = conv_layer(output,filters,[kernel_size,kernel_size],stride,
                             None, training_ph,'instance',padding='valid')
     return x + output
+
+def conv_3Dlayer(x,filters,kernel_size,strides,activation,training_ph,batch_norm=None):
+    output = tf.layers.conv3d(
+                inputs=x,
+                filters=filters,
+                kernel_size=kernel_size,
+                strides=strides,
+                padding='same',
+                activation=None,
+                kernel_initializer=xavier_initializer(),
+                bias_initializer=tf.zeros_initializer(),
+            )
+    if batch_norm == 'batch':
+        output = tf.layers.batch_normalization(
+                    inputs=output,
+                    training=training_ph)
+    elif  batch_norm == 'instance':
+        output = tf.contrib.layers.instance_norm(
+                inputs=output)
+    if activation is not None:
+        output = activation(output)
+    return output
+
+def conv_transpose_3Dlayer(x,filters,kernel_size,strides,
+                         activation,training_ph,batch_norm='batch',padding='same'):
+    output = tf.layers.conv3d_transpose(
+                    inputs=x,
+                    filters=filters,
+                    kernel_size=kernel_size,
+                    strides=strides,
+                    padding='same',
+                    activation=None,
+                    kernel_initializer=xavier_initializer(),
+                )
+    if batch_norm == 'batch':
+        output = tf.layers.batch_normalization(
+                    inputs=output,
+                    training=training_ph)
+    elif  batch_norm == 'instance':
+        output = tf.contrib.layers.instance_norm(
+                inputs=output)
+    if activation is not None:
+        output = activation(output)
+    return output
